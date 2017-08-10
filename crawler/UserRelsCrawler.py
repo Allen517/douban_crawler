024b3417ae40a6c5780bcfc2c17521a9c4a43900
King Wang <wangyongqing.casia@gmail.com>
2017-08-10 15:06:05 +0800

    fix bugs @8/10

--- crawler/UserRelsCrawler.py
+++ crawler/UserRelsCrawler.py
@@ -35,15 +35,16 @@ class UserRelsCrawler(object):
 		self.db_user = MongodbDoubanUsers()
 		self.proxy = proxy
 		self.retry_num = 0 
-		self.max_retry = 1
+		self.max_retry = 2
 		self.fail_count = 0
 		self.max_fail_count = 3
 		self.log = LogHandler('crawl_user_rels')
-		self.log.info(u"当前使用代理%s"%proxy)
+		self.sleep_time = 3
+		# self.log.info(u"当前使用代理%s"%proxy)
 
 	def __getDoubanUserRelsOnePage(self, uid, s):
 		#url = "https://api.douban.com/v2/user/{}"
-		url = "https://api.douban.com/shuo/v2/users/{user}/following?count=50&start={start}".format(user=uid, start=s)
+		url = "https://api.douban.com/shuo/v2/users/{user}/following?count=50&start={start}&apikey=09ec337ecc4d17531dc49a7c64e745f9".format(user=uid, start=s)
 		content = getUrlContent(url, self.proxy)
 		self.retry_num = 0 
 		while not content:
@@ -64,7 +65,7 @@ class UserRelsCrawler(object):
 	def __getDoubanUserRels(self, uid):
 		s = 0
 		rels = tuple(self.__getDoubanUserRelsOnePage(uid, s))
-		sleep(24) #休眠25秒后继续抓取
+		sleep(self.sleep_time) #休眠25秒后继续抓取
 		if not rels:
 			self.fail_count += 1
 			self.log.info(u"当前用户%s关注关系抓取失败！"%uid)
@@ -76,7 +77,7 @@ class UserRelsCrawler(object):
 			if not cur_rels:
 				break
 			rels += cur_rels
-			sleep(24) #休眠25秒后继续抓取
+			sleep(self.sleep_time) #休眠25秒后继续抓取
 		self.log.info(u"已完成用户%s的关注关系抓取"%uid)
 		return rels
 
@@ -101,41 +102,49 @@ class UserRelsCrawler(object):
 						self.db_user.putUser('{{"uid":"{id}", "has_crawled":{crawled}, "has_got_rels":{got_rels}}}'\
 											.format(id=uid_in_rel, crawled=0, got_rels=0))
 					self.log.info(u"已将用户%s的关注用户加入用户表"%uid)
+				if not rels and self.retry_num<self.max_retry:
+					self.db_user.updateUserRelsCrawlTag(uid, 1)
+					self.log.info(u"用户%s的关注用户信息无法抓取"%uid)
 			if u_index<0 or self.retry_num>=self.max_retry or self.fail_count>=self.max_fail_count:
 				break
-		self.log.info(u"当前的用户关注关系抓取线程任务已完成")
+		self.log.info(u"当前的用户%s关注关系抓取线程任务已完成"%uid)
 
 def userRelsCrawler(proxy):
-	pm = ProxyManager()
-	pm.useProxy(proxy)
+	# pm = ProxyManager()
+	# pm.useProxy(proxy)
 	urc = UserRelsCrawler(proxy)
 	urc.getDoubanUserRels()
 	if urc.retry_num<urc.max_retry and urc.fail_count<urc.max_fail_count:
-		pm.releaseProxy(proxy)
+		# pm.releaseProxy(proxy)
 		urc.log.info(u"释放当前的代理%s"%proxy)
 	else:
 		urc.log.info(u"当前的代理%s失效"%proxy)
 
-def main(process_num=5):
-	pm = ProxyManager()
+def main(process_num=10):
+	# pm = ProxyManager()
 	mdu = MongodbDoubanUsers()
 	global uncrawled_rels_users, uru_index, len_uru
 	uncrawled_rels_users = uncrawled_rels_users[uru_index:]
 	uncrawled_rels_users.extend(mdu.getUsersRelCrawled(0))
 	len_uru = len(uncrawled_rels_users)
-	unused_proxies = pm.getAllUnusedProxies()
+	# unused_proxies = pm.getAllUnusedProxies()
 	#1. 取出当前可用的代理
 	#2. 取出当前需要抓取用户关系的用户列表
 	#3. 利用代理获取用户列表中用户的关注关系
 	pl = []
-	proc_num = 0
-	for proxy,used in unused_proxies:
-		if used==0:
-			proc_num += 1
-			if proc_num>process_num:
-				break
-			proc = Thread(target=userRelsCrawler, args=(proxy,))
-			pl.append(proc)
+	proxy = ""
+	for proc_num in range(process_num):
+		proc = Thread(target=userRelsCrawler, args=(proxy,))
+		pl.append(proc)
+
+	# proc_num = 0
+	# for proxy,used in unused_proxies:
+	# 	if used==0:
+	# 		proc_num += 1
+	# 		if proc_num>process_num:
+	# 			break
+	# 		proc = Thread(target=userRelsCrawler, args=(proxy,))
+	# 		pl.append(proc)
 
 	for num in range(process_num):
 		pl[num].start()
@@ -144,6 +153,7 @@ def main(process_num=5):
 		pl[num].join()
 
 def run():
+	main()
 	sched = BlockingScheduler()
 	sched.add_job(main, 'interval', minutes=1)
 	sched.start()
